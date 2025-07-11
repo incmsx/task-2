36009a38043a28a0569c47bb8790dd684ccbe95d
incmsx <kondratovich2431@gmail.com>
2025-07-11 15:34:00 +0300

    receive and constructor tests add

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  gasReporter: {
+    enabled: true,
+    
+  }
 };
 
 export default config;
--- test/Vault.test.ts
+++ test/Vault.test.ts
@@ -6,18 +6,57 @@ describe("Vault", function() {
     async function deploy() {
         const [user1] = await ethers.getSigners();
 
+        const ownerAddress = user1.address;
+
         const Factory = await ethers.getContractFactory("Vault");
-        const vault = await Factory.deploy(user1);
+        const vault = await Factory.deploy(ownerAddress);
         vault.waitForDeployment();
 
-        return { user1, vault }
+        return { user1, vault, ownerAddress }
     }
 
     it("should be deployed", async function() {
-        const { user1, vault } = await loadFixture(deploy);
+        const { vault } = await loadFixture(deploy);
 
         console.log(vault.target);
     });
+    
 
+    describe("Constructor", function () {
 
+        it("should be proper initalAddress", async function() {
+            const { ownerAddress } = await loadFixture(deploy);
+
+            expect(ownerAddress).to.be.properAddress;
+        });
+    });
+
+    describe("Payble", function () {
+
+        async function receive() {
+            const { vault } = await loadFixture(deploy);
+            const amount = ethers.parseEther("1");
+            
+            const signers = await ethers.getSigners();
+            const user2 = signers[1];
+
+            user2.sendTransaction({
+                to: vault.target,
+                value: amount
+            });
+
+            return { vault, amount }
+         
+        }
+
+        it("should be accept ETH via receive()", async function() {
+            
+            const { vault, amount } = await loadFixture(receive);
+
+            expect(await ethers.provider.getBalance(vault.target) == amount);
+
+        });
+
+    });
+    
 });
