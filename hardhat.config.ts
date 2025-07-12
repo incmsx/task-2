fd6cd9bc37c63b9f5aadb615a981a12ab42d40d9
incmsx <kondratovich2431@gmail.com>
2025-07-13 00:44:18 +0300

    gasReporter configurated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-gas-reporter"
+import "hardhat-gas-reporter";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   gasReporter: {
     enabled: true,
-    
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY, 
+    token: "ETH",
+    gasPrice: 50
   }
 };
 
